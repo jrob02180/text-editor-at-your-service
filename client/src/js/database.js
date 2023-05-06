072e185ad75b819abbdbf822ad23d3d4db26f291
jrob02180 <jrob02180@gmail.com>
2023-05-05 21:24:09 -0600

    Still making changes to putDb and getDb, still getting errors.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,22 +19,22 @@ export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
-  return result;
+  // return result;
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("getDb not implemented");
+  // console.error("getDb not implemented");
 
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result?.value;
 };
 
 initdb();
